530a24cfc8be331444f77dcf50b52cec35bde440
niall <niall@solidstategroup.com>
2018-06-26 19:08:53 +0100

    react map component with static pointer

--- web/components/pages/HomePage.js
+++ web/components/pages/HomePage.js
@@ -1,24 +1,58 @@
-import React from "react";
-
-module.exports = class extends React.Component {
-    static contextTypes = {
-        router: React.PropTypes.object.isRequired
-    };
-
-    constructor(props, context) {
-        super(props, context);
-        this.state = {};
+import React, {Component} from 'react';
+import ReactMapGL, {Marker, NavigationControl} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+const TOKEN = 'pk.eyJ1IjoibmlhbGwtc3NnIiwiYSI6ImNpZXd2ZXkwdDAwMGJ1Z20wbnMxZnN1NmcifQ.yrI7TYFTV2mlzvWV0kI71A';
+const navStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: '10px'
+};
+export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                width: 1000,
+                height: 500,
+                latitude: 37.7577,
+                longitude: -122.4376,
+                zoom: 0
+            },
+            popupInfo: null
+        };
     }
 
-    componentWillMount() {
 
-    }
-
-    render = () => {
+    render() {
+        const {viewport} = this.state;
         return (
             <div>
-                Testing a deploy
+                <div className="row">
+                    <div className="col-md-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-10">
+                        <ReactMapGL
+                            {...viewport}
+                            onViewportChange={(viewport) => this.setState({viewport})}
+                            mapStyle="mapbox://styles/mapbox/dark-v9"
+                            mapboxApiAccessToken={TOKEN}>
+                            <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
+                                <div style={{color: 'white'}}>You are here</div>
+                            </Marker>
+                        </ReactMapGL>
+                    </div>
+                </div>
             </div>
         );
     }
-};
+}
